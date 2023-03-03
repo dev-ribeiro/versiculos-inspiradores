602a1bf35a197ee0107d8dfb46331e54df020049
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-03-03 17:56:35 -0300

    fix: include italic font and link

--- src/themes/default/index.ts
+++ src/themes/default/index.ts
@@ -2,11 +2,15 @@ export const defaultTheme = {
     COLORS: {
         BLACK: '#000000',
         WHITE: '#ffffff',
+
+        GRAY_100: '#F3F4F6',
+
         BLUE_400: '#60A5FA',
     },
 
     FONT_FAMILY: {
         REGULAR: 'Roboto-Regular',
         BOLD: 'Roboto-Bold',
+        ITALIC: 'Roboto-Italic',
     },
 } as const;
